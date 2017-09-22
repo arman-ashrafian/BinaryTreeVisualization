a1dbd31259f79be2068286f9c2e523b97bcfd00c
Arman Ashrafian <ashy63@gmail.com>
2017-09-22 11:26:05 -0700

    adds nodes and draws em

--- binary_tree.js
+++ binary_tree.js
@@ -1,27 +1,72 @@
 var window_width = $(document).width();
 var screen_mid = Math.floor(window_width/2);
 
-function Node(val, parent, l_child, r_child, x, y) {
+function Node(val, l_child, r_child, x, y) {
     this.val = val;
-    this.parent_node = parent;
     this.left_child = l_child;
     this.right_child = r_child;
-    this.x_pos = x;
-    this.y_pos = y;
+    this.x = x;
+    this.y = y;
+
+    // Recursively add Node children
+    this.addNode = function(val, level) {
+        level += level + 1;
+        if(val < this.val) {
+            if(this.left_child == null) {
+                this.left_child = new Node(val, null, null, this.x - (400/level), this.y + 100);
+                line(this.x, this.y, this.x - (400/level), this.y + 100)
+            } else {
+                this.left_child.addNode(val, level);
+            }
+        } else {
+            if(this.right_child == null) {
+                this.right_child = new Node(val, null, null, this.x + (400/level), this.y + 100);
+                line(this.x, this.y, this.x + (400/level), this.y + 100)
+            } else {
+                this.right_child.addNode(val, level);
+            }
+        }
+    }
+
+    this.visit = function() {
+        if(this.left_child != null) {
+            this.left_child.visit();
+        }
+        console.log(this.val);
+
+        fill(0);
+        ellipse(this.x + 12, this.y - 10, 50,50);
+        fill(255);
+        textSize(24);
+        text(this.val.toString(), this.x, this.y);
+
+        if(this.right_child != null) {
+            this.right_child.visit();
+        }
+    }
 }
 
 function BinaryTree() {
     this.root = null;
 
-    this.addNode = function(val) {
+    this.addValue = function(val) {
         if(this.root === null) {
-            this.root = new Node(val, null, null, null,
-                                screen_mid,100)
+            this.root = new Node(val, null, null, screen_mid,100);
+        } else {
+            this.root.addNode(val, 0);
         }
+    }
+
+    this.traverse = function() {
+        this.root.visit();
     }
 
 }
 
-var bt = new BinaryTree();
-bt.addNode(5);
-console.log(bt)
+// var bt = new BinaryTree();
+// bt.addValue(5);
+// bt.addValue(4);
+// bt.addValue(7);
+// bt.addValue(10);
+// bt.addValue(12);
+// console.log(bt);
--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 function setup() {
+    // Canvas setup
     var canvas = createCanvas($(document).width(), 5000);
     canvas.parent('sketch');
-
     background(200, 200, 200);
+
+    var tree = new BinaryTree();
+    tree.addValue(11);
+    tree.addValue(5);
+    tree.addValue(4);
+    tree.addValue(6);
+    tree.addValue(3);
+    tree.addValue(20);
+    tree.addValue(25);
+    tree.addValue(22);
+
+    tree.traverse();
 }
 
 function draw() {
@@ -11,11 +23,17 @@ function draw() {
 
     var parent_y = 100;
 
-    ellipse(screen_mid, parent_y, 50, 50);
+    // // root
+    // ellipse(screen_mid, parent_y, 50, 50);
+    //
+    // // left child
+    // ellipse(screen_mid - 200, parent_y + 100, 50, 50);
+    // // right child
+    // ellipse(screen_mid + 200, parent_y + 100, 50, 50);
+    //
+    // line(screen_mid, parent_y, screen_mid - 200, parent_y + 100);
+    // line(screen_mid, parent_y, screen_mid + 200, parent_y + 100);
 
-    ellipse(screen_mid - 200, parent_y + 100, 50, 50);
-    ellipse(screen_mid + 200, parent_y + 100, 50, 50);
 
-    line(screen_mid, parent_y, screen_mid - 200, parent_y + 100);
-    line(screen_mid, parent_y, screen_mid + 200, parent_y + 100);
+
 }
